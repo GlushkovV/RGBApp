6aa29e3843c03778080816ec5c68a68626098d83
Vitaly Glushkov <vit8@live.ru>
2022-05-09 21:39:09 +0400

    Initial Commit

---
+++ RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by Vitaly Glushkov on 09.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
